399c0f27712380a844958e828cf8a985e55b2417
lucas badico <lucasgomesdesantana@gmail.com>
2023-01-27 11:07:54 -0300

    refac: replace sqs for rabbitmq

--- infra/aws-sqs.go
+++ infra/aws-sqs.go
@@ -1,69 +1,110 @@
 package infra
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"log"
+	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/sqs"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 type Queue struct {
-	client *sqs.SQS
+	conn   *amqp.Connection
+	ch     *amqp.Channel
+	LogQ   amqp.Queue
+	EventQ amqp.Queue
 }
 
 func NewQueue() *Queue {
 	return &Queue{}
 }
 
-func (q *Queue) Setup(region, clientId, clientSecret string) error {
-	c := credentials.NewStaticCredentials(clientId, clientSecret, "")
-	sess, err := session.NewSession(
-		&aws.Config{
-			Region:      aws.String(region),
-			Credentials: c,
-		},
+func (q *Queue) Setup(username, password, serverUrl string) error {
+	uri := fmt.Sprintf("amqps://%s:%s@%s", username, password, serverUrl)
+	fmt.Println(uri)
+	conn, err := amqp.Dial(uri)
+	if err != nil {
+		log.Panicf("%s: %s", "Failed to connect to RabbitMQ", err)
+	}
+	// defer conn.Close()
+
+	q.conn = conn
+	ch, err := q.conn.Channel()
+	q.ch = ch
+	logQ, err := ch.QueueDeclare(
+		"LogMessages", // name
+		true,          // durable
+		false,         // delete when unused
+		false,         // exclusive
+		false,         // no-wait
+		nil,           // arguments
 	)
-	if err != nil {
-		fmt.Println("Error creating session:", err)
-		return err
-	}
-	client := sqs.New(sess)
-	q.client = client
+	q.LogQ = logQ
+
+	// err check
+
+	eventQ, err := ch.QueueDeclare(
+		"EventMessages", // name
+		true,            // durable
+		false,           // delete when unused
+		false,           // exclusive
+		false,           // no-wait
+		nil,             // arguments
+	)
+
+	// err check
+
+	q.EventQ = eventQ
 	return nil
 }
 
-func (q *Queue) SendMessage(queueURL string, message string) error {
+func (q *Queue) SendMessage(nameq string, data interface{}) error {
 	fmt.Println("========================")
-	fmt.Println("SendMessage:", queueURL)
+	fmt.Println("SendMessage to ", nameq)
 	fmt.Println("========================")
-	req, resp := q.client.SendMessageRequest(&sqs.SendMessageInput{
-		MessageBody: aws.String(message),
-		QueueUrl:    aws.String(queueURL),
-	})
-	err := req.Send()
+	message, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("============================================")
+		fmt.Println("WARN: CLOUDLOG ENGINE NOT WORKING")
+		fmt.Println("============================================")
+		fmt.Println(err)
+		return err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*50)
+	defer cancel()
+	err = q.ch.PublishWithContext(ctx,
+		"",    // exchange
+		nameq, // routing key
+		false, // mandatory
+		false, // immediate
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        message,
+		})
+
 	if err != nil {
 		fmt.Println("Error sending message:", err)
 		return err
 	}
-	fmt.Println("Resp: ", resp)
 	return nil
 }
 
-func (q *Queue) ConsumeMessages(queueURL, message string) error {
-	fmt.Println("========================")
-	fmt.Println("SendMessage:", queueURL)
-	fmt.Println("========================")
-	req, resp := q.client.SendMessageRequest(&sqs.SendMessageInput{
-		MessageBody: aws.String(message),
-		QueueUrl:    aws.String(queueURL),
-	})
-	err := req.Send()
-	if err != nil {
-		fmt.Println("Error sending message:", err)
-		return err
-	}
-	fmt.Println("Resp: ", resp)
-	return nil
-}
+// func (q *Queue) ConsumeMessages(queueURL string) error {
+// 	fmt.Println("========================")
+// 	fmt.Println("SendMessage:", queueURL)
+// 	fmt.Println("========================")
+// 	req, resp := q.client.SendMessageRequest(&sqs.SendMessageInput{
+// 		MessageBody: aws.String(message),
+// 		QueueUrl:    aws.String(queueURL),
+// 	})
+// 	err := req.Send()
+// 	if err != nil {
+// 		fmt.Println("Error sending message:", err)
+// 		return err
+// 	}
+// 	fmt.Println("Resp: ", resp)
+// 	return nil
+// }
